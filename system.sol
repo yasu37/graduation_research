0ca591d582b92fa0526dcf86aa326eb9a95cf193
kan <75612603+yasu37@users.noreply.github.com>
2024-01-27 13:35:47 +0900

    Update system.sol

--- system.sol
+++ system.sol
@@ -4,18 +4,14 @@ pragma solidity ^0.8.14;
 
 contract System {
     struct TransactionInfo {
-        /// @dev BCに登録する値を格納する構造体
-        string[] encryptedVerification;               /// 暗号化した確認用情報を格納する配列
-        bool validated;           /// @dev 荷物情報が確認済みかどうかの確認に使用する
-        uint transactionNo;       /// @dev 荷物番号
-        uint shipTimes;           /// @dev 経由回数
+        // BCに登録する値を格納する構造体
+        bytes32[] Verification;             // 確認用情報[0]を格納する配列
+        string[] encryptedVerification;     // 暗号化した確認用情報[n]を格納する配列
+        bool validated;                     // 荷物情報が確認済みかどうかの確認に使用する
+        uint transactionNo;                 // 荷物番号
+        uint shipTimes;                     // 経由回数
     }
 
-    uint transactionCount = 1; /// @dev 取引の数を格納する変数 -> 荷物番号の割り振りに使用する
+    mapping (uint => TransactionInfo) identInfoToTransactionInfo; // 荷物番号から確認用情報の構造体を検索するマッピング
 
-    mapping (uint => TransactionInfo) identInfoToTransactionInfo; /// @dev 識別情報(荷物番号)から確認用情報の構造体を検索するマッピング
-    mapping (uint => uint) transactionNoToId;                            /// @dev 荷物番号から特徴量を検索するマッピング
-
-
-
-} 
+}
