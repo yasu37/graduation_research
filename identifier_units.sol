ce62e0115a17d61b11c82a04ba76b4dfeb02c7a6
kan <75612603+yasu37@users.noreply.github.com>
2024-01-27 13:34:33 +0900

    Update identifier_units.sol

--- identifier_units.sol
+++ identifier_units.sol
@@ -2,46 +2,45 @@
 
 pragma solidity ^0.8.14;
 
-import "./hash_generator.sol";
+import "./hash_generator0.sol";
 
-/// @dev 受取人の処理のコントラクト
-contract IdentifierUnits is HashGenerator {
-    ///受取人は暗号化された確認用情報をBCから取得する
-    function getValidation(uint correctId) public view returns (string memory, string memory, string memory) {
-        uint transactionNo = correctId;
+// 受取人の処理
+contract RecipientUnits is HashGenerator {
+    function verification(uint labelId, address sender, address receiver, uint transactionNo) public returns (string memory, bytes32, bytes32) {
 
-        return (identInfoToTransactionInfo[transactionNo].encryptedVerification[0],
-                identInfoToTransactionInfo[transactionNo].encryptedVerification[1],
-                identInfoToTransactionInfo[transactionNo].encryptedVerification[2]
-        );
-    }
+        // 確認用情報[0]と[0]'を格納する配列
+        // 確認用情報[0]’を生成
+        bytes32[2] memory Validation0 =  [identInfoToTransactionInfo[transactionNo].Verification[0], generate0(labelId, sender, receiver, transactionNo)];
 
-       ///真贋判定関数  復号化した確認用情報と配送者が生成した確認用情報を比較する。
-    function identify(uint labelId, address sender, address receiver, uint transactionNo, bytes32 sendValidation1, bytes32 sendValidation2,bytes32 decryptValidation0, bytes32 decryptValidation1, bytes32 decryptValidation2, uint shipTimes) public returns (string memory, uint) {
-        //受取人が配送者から送られてきた確認用情報の配列
-        bytes32[2] memory sendValidation =  [sendValidation1, sendValidation2];
-        //受取人が復号化した確認用情報の配列
-        bytes32[3] memory decryptValidation =  [decryptValidation0, decryptValidation1, decryptValidation2];
-
-       //問題が起こった配送者
-        uint problemDeliver=0;
-
-        if(decryptValidation[0] == generate0(labelId, sender, receiver, transactionNo)) {
+        // 確認用情報[0]と[0]'を比較
+        if(Validation0[0] == Validation0[1]) {
+            // 成功した場合
             identInfoToTransactionInfo[transactionNo].validated = true;
-            return ("Validation succeeded!!", 0);
+            return ("Validation succeeded!!", Validation0[0], Validation0[1]);
         } else {
-            ///配送者１　→ 配送者２の順番でチェック
-            for(uint i=0; i<=shipTimes; i++){
-                ///配送者が受取人に送った確認用情報
-                if(sendValidation[i] == decryptValidation[i+1]) {
-                    problemDeliver++;
-                } else {
-                    break;
-                }
-            }
-            return ("Validation failed!!", problemDeliver);
+            // 失敗した場合
+            return ("Validation failed!!", Validation0[0], Validation0[1]);
         }
 
+    }
+
+    // 復号化した確認用情報[n]と配送者から受信した確認用情報[n]を比較する。
+    function identify(bytes32 sendValidation1, bytes32 sendValidation2, bytes32 decryptValidation1, bytes32 decryptValidation2, uint shipTimes) public pure returns (uint) {
+        // 配送者から受信した確認用情報[n]を格納する配列
+        bytes32[2] memory sendValidation =  [sendValidation1, sendValidation2];
+        //　復号化した確認用情報[n]を格納する配列
+        bytes32[2] memory decryptValidation =  [decryptValidation1, decryptValidation2];
+        // 配送問題を引き起こした配送者番号
+        uint problemDeliver=0;
+        ///配送者１　→ 配送者２の順番で比較
+        for(uint i=0; i< shipTimes; i++){
+            if(sendValidation[i] == decryptValidation[i]) {
+                problemDeliver++;
+            } else {
+                break;
+            }
+        }
+        return (problemDeliver);
     }
 
 }
