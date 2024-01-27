4eee7cd7d1f006853ff1fda9ecb09d877436d47f
kan <75612603+yasu37@users.noreply.github.com>
2024-01-27 13:35:12 +0900

    Update register_units.sol

--- register_units.sol
+++ register_units.sol
@@ -2,33 +2,32 @@
 
 pragma solidity ^0.8.14;
 
-import "./hash_generator.sol";
+import "./hash_generator0.sol";
 
-/// @dev 発送者の処理のコントラクト
+/// 発送者の処理
 contract RegisterUnits is HashGenerator {
 
-    ///@dev 全ての確認用情報を生成(例；配送者が2人の場合)
-    function verification(uint correctId, address sender, address receiver, address shipper1, address shipper2, uint shipTimes) external returns (bytes32){
-        /// 荷物番号の決め方の１例(例；固有ラベルIDと同じにする)
+    // 発送者はラベルID、発送者/受取人アドレス、暗号化した確認用情報[n]を送信。  ※配送者が2人いる場合、n=1, 2
+    function registration(uint correctId, address sender, address receiver, string memory encryptedVerification1, string memory encryptedVerification2) external returns (uint){
+        // 荷物番号の生成 ※決め方の１例として、 固有ラベルIDと同じ値。
         uint transactionNo = correctId;
-        identInfoToTransactionInfo[transactionCount].validated = false;
-
-        /// @dev 確認用情報[0]、[1]、[2]を生成 & 受取人に送信
-        return generateN(correctId, shipper2, generateN(correctId, shipper1, generate0(correctId, sender, receiver, transactionNo), shipTimes-1), shipTimes);
+        // 確認用情報[0]を生成
+        bytes32 Verification0 = generate0(correctId, sender, receiver, transactionNo);
+        // 検証結果を登録
+        identInfoToTransactionInfo[transactionNo].validated = false;
+        // 確認用情報[0]を登録
+        identInfoToTransactionInfo[transactionNo].Verification.push(Verification0);
+        // 暗号化した確認用情報[1]を登録
+        identInfoToTransactionInfo[transactionNo].encryptedVerification.push(encryptedVerification1);
+        // 暗号化した確認用情報[2]を登録
+        identInfoToTransactionInfo[transactionNo].encryptedVerification.push(encryptedVerification2);
+        // 荷物番号を発送者と受取人に送信
+        return transactionNo;
     }
 
-    ///暗号化した確認用情報をBCに登録する関数
-    function registration(uint correctID, string memory encryptedVerification1, string memory encryptedVerification2, string memory encryptedVerification3) public returns (string memory, string memory, string memory){
-        uint transactionNo = correctID;
-
-        identInfoToTransactionInfo[transactionNo].encryptedVerification.push(encryptedVerification1);
-        identInfoToTransactionInfo[transactionNo].encryptedVerification.push(encryptedVerification2);
-        identInfoToTransactionInfo[transactionNo].encryptedVerification.push(encryptedVerification3);
-
-        return (identInfoToTransactionInfo[transactionNo].encryptedVerification[0],
-                identInfoToTransactionInfo[transactionNo].encryptedVerification[1],
-                identInfoToTransactionInfo[transactionNo].encryptedVerification[2]
-        );
+    function verificationGenerateN(uint labelid, address shipper, uint shipTimes) public pure returns (bytes32) {
+        // 発送者が確認用情報[n]を生成
+        return keccak256(abi.encodePacked(labelid, shipper, shipTimes));
     }
 
 }
